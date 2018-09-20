85f6b07d438dd96d9311baf753349090c7eee50c
ControlSystem <controlsystem88@hotmail.com>
2018-09-20 12:30:10 -0500

    Comprobando ganador

--- tictac.rb
+++ tictac.rb
@@ -1,4 +1,5 @@
 require 'Matrix'        
+#funcion para mostrar tablero
 def showtable(tab)
     for i in(0..2)
        for j in(0..2)
@@ -7,37 +8,7 @@ def showtable(tab)
        puts
     end
 end
-
-def number_in_x (num, tab)
-    system('cls')
-    tab[0][0]="X" if num == 1
-    tab[0][1]="X" if num == 2
-    tab[0][2]="X" if num == 3
-    tab[1][0]="X" if num == 4
-    tab[1][1]="X" if num == 5
-    tab[1][2]="X" if num == 6
-    tab[2][0]="X" if num == 7
-    tab[2][1]="X" if num == 8
-    tab[2][2]="X" if num == 9
-    showtable(tab)
-    puts    
-end
-
-def number_in_o (num, tab)
-    system('cls')
-    tab[0][0]="O" if num == 1
-    tab[0][1]="O" if num == 2
-    tab[0][2]="O" if num == 3
-    tab[1][0]="O" if num == 4
-    tab[1][1]="O" if num == 5
-    tab[1][2]="O" if num == 6
-    tab[2][0]="O" if num == 7
-    tab[2][1]="O" if num == 8
-    tab[2][2]="O" if num == 9
-    showtable(tab)
-    puts    
-end
-
+#funcion para obtener el juego de X sin que sea una casilla ya marcada
 def get_x(table, selected)
     number=gets.to_i
     puts "this is #{number}"
@@ -55,6 +26,7 @@ def get_x(table, selected)
 
     end
 end
+#funcion para obtener el juego de O sin que sea una casilla ya marcada
 def get_o(table, selected)
     number=gets.to_i
     if selected.include?(number) || number<1
@@ -69,10 +41,74 @@ def get_o(table, selected)
         number_in_o(number, table)
     end
 end
+
+#Insertar valor de X en  tablero y mostrar tablero
+def number_in_x (num, tab)
+    system('cls')
+    tab[0][0]="X" if num == 1
+    tab[0][1]="X" if num == 2
+    tab[0][2]="X" if num == 3
+    tab[1][0]="X" if num == 4
+    tab[1][1]="X" if num == 5
+    tab[1][2]="X" if num == 6
+    tab[2][0]="X" if num == 7
+    tab[2][1]="X" if num == 8
+    tab[2][2]="X" if num == 9
+    showtable(tab)
+    wins?(tab)
+    puts    
+end
+#Insertar valor de O en  tablero y mostrar tablero
+def number_in_o (num, tab)
+    system('cls')
+    tab[0][0]="O" if num == 1
+    tab[0][1]="O" if num == 2
+    tab[0][2]="O" if num == 3
+    tab[1][0]="O" if num == 4
+    tab[1][1]="O" if num == 5
+    tab[1][2]="O" if num == 6
+    tab[2][0]="O" if num == 7
+    tab[2][1]="O" if num == 8
+    tab[2][2]="O" if num == 9
+    showtable(tab)
+    wins?(tab)
+    puts    
+end
+
+#Validacion del ganador en cada ingreso
+def wins?(tab)
+    #Tableros Ganadores
+    x_wins = ["X","X","X"]
+    o_wins = ["O","O","O"]
+
+    #Validacion en diagonal
+    if((0...3).collect {|i| tab[i][i]} == x_wins)
+        puts "X is the Winner!!" 
+    elsif ((0...3).collect {|i| tab[i][i]} == o_wins)
+        puts "O is the Winner!!"
+    elsif ((0...3).collect {|i| tab[i]} == x_wins)  
+    end
+
+
+    #Validacion en diagonal Invertida
+    #if((3...0).collect {|i| tab[i][i]} == x_wins)
+        
+    #elsif ((3...3).collect {|i| tab[i][i]} == o_wins)
+        #puts "O is the Winner!!"   
+    #end
+
+    #Validacion en filas
+    #tab.each do |x| 
+    #   puts "X is the Winner!!" if x==x_wins
+    #    puts "O is the Winner!!" if x=o_wins
+    #end
+
+end
+
+
+#Programa principial
 puts 'Welcome to TicTacToe Logic X/O'
 table = [[1,2,3], [4,5,6], [7,8,9]]
-x_wins = ["X","X","X"]
-o_wins = ["O","O","O"]
 selected= []
 showtable(table)
 puts '|X| Time to play, Put the number you want!'
